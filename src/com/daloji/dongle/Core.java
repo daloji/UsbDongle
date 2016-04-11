8eff5b3b45cbe87827bfc08e9c09164755badbce
daloji <jiantila@gmail.com>
2016-04-11 17:51:03 +0200

    add new method

--- src/com/daloji/dongle/Core.java
+++ src/com/daloji/dongle/Core.java
@@ -1,10 +1,31 @@
 package com.daloji.dongle;
 
+import com.daloji.dongle.exception.InvalidConfigurationException;
+
+/**
+ *  Helper from loading shared library and native methodse
+ * 
+ * @author Johan Boris IANTILA
+ * @since USBDongle 1.0
+ * @see SystemLoadCustom
+ *
+ */
 public class Core {
 	
-	public native void sendSMS(String number,String messages);
+	public native void sendSMS(String phonenumber,String messages);
 
+	public native float getBalanceMobileBanking(String command);
+	
+	public native void transfertMobileBanking(String phonenumber,float amount,String command);
+	
 	static {
-	      System.loadLibrary("libUsbDongle");
+		
+			try {
+				SystemLoadCustom.loadLibrary("lib/libUsbDongle");
+			} catch (InvalidConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 	}
 }
