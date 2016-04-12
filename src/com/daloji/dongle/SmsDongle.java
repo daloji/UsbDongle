c016ecde1479119a8fef18e344689e6547153bdb
daloji <jiantila@gmail.com>
2016-04-12 16:01:39 +0200

    undo jni exception

--- src/com/daloji/dongle/SmsDongle.java
+++ src/com/daloji/dongle/SmsDongle.java
@@ -10,66 +10,51 @@ import com.daloji.dongle.exception.InvalidConfigurationException;
  * @see SmsDongle
  *
  */
- 
+
 public class SmsDongle {
 
 	private Core core;
-   	
+
 	public SmsDongle(){
 		core= new Core();
 	}
 
-	
-    /**
-     * method that allow you to send SMS 
-     * @param number  destination phone number
-     * @param messages text message 
-     * 
-    */
+
+	/**
+	 * method that allow you to send SMS 
+	 * @param number  destination phone number
+	 * @param messages text message 
+	 * 
+	 */
 	public void sendSmS(String number,String messages){
 		if(core !=null){
-			try {
-				core.sendSMS(number, messages);
-			} catch (InvalidConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			core.sendSMS(number, messages);
 		}
 	}
-	
+
 	/**
-     * method that allow you to send SMS 
-     * @param command   ussd  (check your operator phone command)
-     * @return  balance value
-     * 
-    */
+	 * method that allow you to send SMS 
+	 * @param command   ussd  (check your operator phone command)
+	 * @return  balance value
+	 * 
+	 */
 	public float getBalanceMobileBanking(String command){
 		float balance = 0;
 		if(core !=null){
-			try {
-				balance = core.getBalanceMobileBanking(command);
-			} catch (InvalidConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			balance = core.getBalanceMobileBanking(command);
 		}
 		return balance;
 	}
-	
+
 	/**
-     * transfet money to another mobile money account
-     * @param phone number Destination phone number
-     * @param amount value to send
-     * @param command   ussd  (check your operator phone command
-    */
+	 * transfet money to another mobile money account
+	 * @param phone number Destination phone number
+	 * @param amount value to send
+	 * @param command   ussd  (check your operator phone command
+	 */
 	public void transfertMobileBanking(String phonenumber,float amount,String command){
 		if(core !=null){
-			try {
-				core.transfertMobileBanking(phonenumber,amount,command);
-			} catch (InvalidConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			core.transfertMobileBanking(phonenumber,amount,command);
 		}
 	}
 }
