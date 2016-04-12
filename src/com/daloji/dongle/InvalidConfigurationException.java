15ba84a23c06affa04da07688f3f91b5bf800192
daloji <jiantila@gmail.com>
2016-04-12 19:36:22 +0200

    add exception class

---
+++ src/com/daloji/dongle/InvalidConfigurationException.java
@@ -0,0 +1,72 @@
+package com.daloji.dongle;
+
+import com.daloji.dongle.SystemLoadCustom;
+
+/**
+ *  custom exception for all invalid configuration
+ * 
+ * @author Johan Boris IANTILA
+ * @since USBDongle 1.0
+ * @see InvalidConfigurationException
+ *
+ */
+public class InvalidConfigurationException extends Exception
+{
+
+    /** The serialVersionUID. */
+    private static final long serialVersionUID = -4642961908674605132L;
+
+    /**
+     * Constructor.
+     */
+    public InvalidConfigurationException()
+    {
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param arg0
+     */
+    public InvalidConfigurationException(String arg0)
+    {
+        super(arg0);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param cause
+     */
+    public InvalidConfigurationException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param message
+     * @param cause
+     */
+    public InvalidConfigurationException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Constructor.
+     * 
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace
+     */
+    public InvalidConfigurationException(String message, Throwable cause,
+                                         boolean enableSuppression,
+                                         boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
