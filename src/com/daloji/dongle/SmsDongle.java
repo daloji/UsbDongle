21d5abcbb392a7433ad4d06b47df9079fc85db3f
daloji <jiantila@gmail.com>
2016-04-12 14:21:47 +0200

    add method that manage mobile banking ussd

--- src/com/daloji/dongle/SmsDongle.java
+++ src/com/daloji/dongle/SmsDongle.java
@@ -1,5 +1,14 @@
 package com.daloji.dongle;
 
+/**
+ *   this class manager all command  for USB Dongle
+ * 
+ * @author Johan Boris IANTILA
+ * @since USBDongle 1.0
+ * @see SmsDongle
+ *
+ */
+ 
 public class SmsDongle {
 
 	private Core core;
@@ -9,10 +18,41 @@ public class SmsDongle {
 	}
 
 	
-
+    /**
+     * method that allow you to send SMS 
+     * @param number  destination phone number
+     * @param messages text message 
+     * 
+    */
 	public void sendSmS(String number,String messages){
 		if(core !=null){
 			core.sendSMS(number, messages);
+		}
+	}
+	
+	/**
+     * method that allow you to send SMS 
+     * @param command   ussd  (check your operator phone command)
+     * @return  balance value
+     * 
+    */
+	public float getBalanceMobileBanking(String command){
+		float balance = 0;
+		if(core !=null){
+			balance = core.getBalanceMobileBanking(command);
+		}
+		return balance;
+	}
+	
+	/**
+     * transfet money to another mobile money account
+     * @param phone number Destination phone number
+     * @param amount value to send
+     * @param command   ussd  (check your operator phone command
+    */
+	public void transfertMobileBanking(String phonenumber,float amount,String command){
+		if(core !=null){
+			core.transfertMobileBanking(phonenumber,amount,command);
 		}
 	}
 }
